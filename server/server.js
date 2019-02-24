ed82b1fc69f5bb1810c2a020aca7b8cc1b222b3d
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-02-24 10:16:16 +0200

    init

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var app=express();
 require('./config/middleware.js')(app,express);
 require('./config/routes.js')(app,express);
 
-var mongoURI = "mongodb://Fatima:1234@ds157342.mlab.com:57342/prep" || 'mongodb://localhost/prep';
+var mongoURI = "mongodb://RBK:Thaki2018@ds157342.mlab.com:57342/prep" || 'mongodb://localhost/prep';
 
 mongoose.connect(mongoURI);
 
